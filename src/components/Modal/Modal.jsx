603c658d6b08502baa206a5503ec22aa26b56181
KuhaiAnna <kuhai.anna@gmail.com>
2023-07-17 03:27:10 +0300

    Create Modal.jsx

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+import { useEffect, useCallback } from 'react';
+import { createPortal } from 'react-dom';
+import css from './Modal.module.css';
+import { IconContext } from 'react-icons';
+import { IoMdClose } from 'react-icons/io';
+import { IconButton } from 'components/IconButton/IconButton';
+
+// Create portal
+const modalRoot = document.querySelector('#modal-root');
+
+export const Modal = ({ children, onClose }) => {
+  const hendleKeyDown = useCallback(
+    e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose]
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', hendleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', hendleKeyDown);
+    };
+  }, [hendleKeyDown]);
+
+  const hendleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <div className={css.backdrop} onClick={hendleBackdropClick}>
+      <div className={css.modal}>
+        <IconContext.Provider
+          value={{
+            size: '20px',
+          }}
+        >
+          <IconButton aria-label="Close button" onClick={onClose}>
+            <IoMdClose />
+          </IconButton>
+        </IconContext.Provider>
+        {children}
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.any.isRequired,
+};
