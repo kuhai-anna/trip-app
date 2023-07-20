a7c13134c453db6defcfe5aa617e29b99e5ffa01
KuhaiAnna <kuhai.anna@gmail.com>
2023-07-20 07:23:19 +0300

    Delete App.css from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 import Home from 'pages/Home';
 import { WeatherForecast } from 'components/WeatherForecast/WeatherForecast';
 
