f455b74dd3fd48f1568587cb12aed2eded315926
KuhaiAnna <kuhai.anna@gmail.com>
2023-07-20 07:24:45 +0300

    Add icon to the AddButton

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -1,9 +1,19 @@
 import PropTypes from 'prop-types';
+import { IconContext } from 'react-icons';
+import { BiPlus } from 'react-icons/bi';
+import css from './AddButton.module.css';
 
 export const AddButton = ({ onClick }) => {
   return (
-    <button type="button" onClick={onClick}>
-      Add trip
+    <button className={css.addBtn} type="button" onClick={onClick}>
+      <IconContext.Provider
+        value={{
+          size: '20px',
+        }}
+      >
+        <BiPlus />
+      </IconContext.Provider>
+      <span className={css.addBtnText}>Add trip</span>
     </button>
   );
 };
