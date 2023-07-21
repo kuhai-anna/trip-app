4980fac0ae657c6b734020437eaff4cc99b5b625
KuhaiAnna <kuhai.anna@gmail.com>
2023-07-21 14:33:39 +0300

    Add dynamic icon import

--- src/components/WeatherList/WeatherListItem/WeatherListItem.jsx
+++ src/components/WeatherList/WeatherListItem/WeatherListItem.jsx
@@ -1,16 +1,43 @@
 import PropTypes from 'prop-types';
-import { getDayOfWeek } from 'timer/getDayOfWeek';
+import { useEffect, useState } from 'react';
+import { getDayOfWeek } from 'helpers/getDayOfWeek';
+import css from './WeatherListItem.module.css';
 
 export const WeatherListItem = ({ date, tempmax, tempmin, icon }) => {
+  const [weatherIcon, setWeatherIcon] = useState(null);
   const dayOfWeek = getDayOfWeek(date);
+
+  useEffect(() => {
+    if (icon) {
+      const loadImage = async () => {
+        // Dynamic icon import
+        try {
+          const iconModule = await import(`icons/${icon}.svg`);
+          setWeatherIcon(iconModule.default);
+        } catch (error) {
+          console.error('Error loading city image:', error);
+        }
+      };
+
+      loadImage();
+    }
+  }, [icon]);
 
   return (
     <li>
-      <p>{dayOfWeek}</p>
-      <p>{icon}</p>
-      <p>
-        {tempmax}&deg;/{tempmin}&deg;
-      </p>
+      <div className={css.weatherCard}>
+        <p className={css.dayOfWeek}>{dayOfWeek}</p>
+        <img
+          className={css.weatherIcon}
+          src={`${weatherIcon}`}
+          alt={`${icon} icon`}
+          width={'70'}
+          height={'70'}
+        />
+        <p className={css.temperature}>
+          {Math.round(tempmax)}&deg;/{Math.round(tempmin)}&deg;
+        </p>
+      </div>
     </li>
   );
 };
