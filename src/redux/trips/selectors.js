6a8b98495c284f7d819cbbd31de9ad27cf56139b
KuhaiAnna <kuhai.anna@gmail.com>
2023-07-21 14:07:00 +0300

    Add selectActiveTrip to selectors

--- src/redux/trips/selectors.js
+++ src/redux/trips/selectors.js
@@ -3,13 +3,22 @@ import { selectFilter } from 'redux/filter/selectors';
 
 export const selectTrips = state => state.trips;
 
+export const selectActiveTripId = state => state.trips.activeTripId;
+
 export const selectVisibleTrips = createSelector(
   [selectTrips, selectFilter],
   (trips, filter) => {
     const normalizeFilter = filter?.toLowerCase();
 
-    return trips?.filter(({ city }) =>
+    return trips?.items.filter(({ city }) =>
       city?.toLowerCase().includes(normalizeFilter)
     );
+  }
+);
+
+export const selectActiveTrip = createSelector(
+  [selectTrips, selectActiveTripId],
+  (trips, activeTripId) => {
+    return trips?.items.find(trip => trip.id === activeTripId);
   }
 );
