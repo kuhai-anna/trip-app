831ea6255decb22f48a18d085b07ce4174c08aa9
KuhaiAnna <kuhai.anna@gmail.com>
2023-07-21 16:06:06 +0300

    Update basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="/trip-app">
           <App />
         </BrowserRouter>
       </PersistGate>
